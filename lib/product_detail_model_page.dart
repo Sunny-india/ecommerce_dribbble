a8f8337e3cbe87444af3235e2bdd6a867076c2e7
sunnychawla <shrichawlaplastics14@gmail.com>
2023-09-28 10:01:41 +0530

    page view builder created

--- lib/product_detail_model_page.dart
+++ lib/product_detail_model_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'color_constants.dart';
@@ -19,6 +18,8 @@ class _ProductDetailModelPageState extends State<ProductDetailModelPage> {
     'assets/images/shoe2.jpeg',
     'assets/images/shoe3.webp'
   ];
+  int pageNumber = 0;
+  PageController pageController = PageController();
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.sizeOf(context);
@@ -33,24 +34,52 @@ class _ProductDetailModelPageState extends State<ProductDetailModelPage> {
               child: Container(
                 height: size.height * .5,
                 width: size.width,
-                decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.only(
-                      bottomLeft: Radius.circular(12),
-                      bottomRight: Radius.circular(12),
-                    ),
-                    border: Border.all(color: CupertinoColors.black)),
+                // decoration: BoxDecoration(
+                //     borderRadius: const BorderRadius.only(
+                //       bottomLeft: Radius.circular(12),
+                //       bottomRight: Radius.circular(12),
+                //     ),
+                //     border: Border.all(color: CupertinoColors.black)),
                 child: PageView.builder(
+                    scrollDirection: Axis.horizontal,
+                    controller: pageController,
                     itemCount: images.length,
+                    onPageChanged: (int changedPage) {
+                      setState(() {
+                        pageNumber = changedPage;
+                      });
+                    },
                     itemBuilder: (context, index) {
                       return Container(
                         child: Image(
                           image: AssetImage(
-                            images[index].toString(),
+                            images[index % images.length].toString(),
                           ),
                           fit: BoxFit.cover,
                         ),
                       );
                     }),
+              ),
+            ),
+            Positioned(
+              top: size.height * .55,
+              left: size.width * .25,
+              child: Row(
+                children: List.generate(images.length, (index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 6.0),
+                    child: InkWell(
+                      onTap: () {
+                        pageController.jumpToPage(index);
+                      },
+                      child: CircleAvatar(
+                        radius: 8,
+                        backgroundColor:
+                            pageNumber == index ? Colors.amber : Colors.teal,
+                      ),
+                    ),
+                  );
+                }),
               ),
             ),
           ],
