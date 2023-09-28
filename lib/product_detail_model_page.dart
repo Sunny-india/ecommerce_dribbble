7783af11ed1b5bdecd839c10533b60dc8074179d
sunnychawla <shrichawlaplastics14@gmail.com>
2023-09-28 20:43:00 +0530

    working on Down Container

--- lib/product_detail_model_page.dart
+++ lib/product_detail_model_page.dart
@@ -31,60 +31,80 @@ class _ProductDetailModelPageState extends State<ProductDetailModelPage> {
             Positioned(
               top: 0,
               left: 0,
-              child: Container(
-                height: size.height * .5,
-                width: size.width,
-                // decoration: BoxDecoration(
-                //     borderRadius: const BorderRadius.only(
-                //       bottomLeft: Radius.circular(12),
-                //       bottomRight: Radius.circular(12),
-                //     ),
-                //     border: Border.all(color: CupertinoColors.black)),
-                child: PageView.builder(
-                    scrollDirection: Axis.horizontal,
-                    controller: pageController,
-                    itemCount: images.length,
-                    onPageChanged: (int changedPage) {
-                      setState(() {
-                        pageNumber = changedPage;
-                      });
-                    },
-                    itemBuilder: (context, index) {
-                      return Container(
-                        child: Image(
-                          image: AssetImage(
-                            images[index % images.length].toString(),
-                          ),
-                          fit: BoxFit.cover,
-                        ),
-                      );
-                    }),
-              ),
+              child: buildSizedBoxWithPageView(size),
             ),
             Positioned(
-              top: size.height * .55,
+              top: size.height * .45,
               left: size.width * .25,
-              child: Row(
-                children: List.generate(images.length, (index) {
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 6.0),
-                    child: InkWell(
-                      onTap: () {
-                        pageController.jumpToPage(index);
-                      },
-                      child: CircleAvatar(
-                        radius: 8,
-                        backgroundColor:
-                            pageNumber == index ? Colors.amber : Colors.teal,
-                      ),
-                    ),
-                  );
-                }),
-              ),
+              child: buildRowForPageControllingDots(),
             ),
+            Positioned(
+              //todo: how to set width and height when landscape in all widgets
+              top: size.height * .5,
+              child: buildDownContainer(size),
+            )
           ],
         ),
       ),
+    );
+  }
+
+  SizedBox buildSizedBoxWithPageView(Size size) {
+    return SizedBox(
+      height: size.height * .5,
+      width: size.width,
+      child: PageView.builder(
+          scrollDirection: Axis.horizontal,
+          controller: pageController,
+          itemCount: images.length,
+          onPageChanged: (int changedPage) {
+            setState(() {
+              pageNumber = changedPage;
+            });
+          },
+          itemBuilder: (context, index) {
+            return Image(
+              image: AssetImage(
+                images[index % images.length].toString(),
+              ),
+              fit: BoxFit.cover,
+            );
+          }),
+    );
+  }
+
+  Row buildRowForPageControllingDots() {
+    return Row(
+      children: List.generate(images.length, (index) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 6.0),
+          child: InkWell(
+            onTap: () {
+              pageController.jumpToPage(index);
+            },
+            child: Container(
+              width: 24,
+              height: 12,
+              decoration: BoxDecoration(
+                borderRadius:
+                    pageNumber == index ? BorderRadius.circular(12) : null,
+                shape:
+                    pageNumber == index ? BoxShape.rectangle : BoxShape.circle,
+                color: pageNumber == index ? Colors.black : Colors.grey,
+              ),
+            ),
+          ),
+        );
+      }),
+    );
+  }
+
+  Container buildDownContainer(Size size) {
+    return Container(
+      height: size.height * .5,
+      width: size.width,
+      color: Colors.red,
+      child: Column(),
     );
   }
 }
